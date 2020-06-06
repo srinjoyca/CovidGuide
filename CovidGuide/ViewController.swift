8f6a3420ee6ba9c79df66bad8765dd140430e0cd
srinjoy dutta <srinjoy.dutta@icloud.com>
2020-06-05 22:46:18 -0700

    Removed unwanted code

--- CovidGuide/ViewController.swift
+++ CovidGuide/ViewController.swift
@@ -25,36 +25,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        let sourceLocation = CLLocationCoordinate2D(latitude: 39.173209, longitude: -94.593933)
-        let destinationLocation = CLLocationCoordinate2D(latitude: 38.643172, longitude: -90.177429)
-        let sourcePin = customPin(pinTitle: "Kansas City", pinSubTitle: "", location: sourceLocation)
-        let destPin = customPin(pinTitle: "St. Louis", pinSubTitle: "", location: destinationLocation)
-        self.mapView.addAnnotation(sourcePin)
-       self.mapView.addAnnotation(destPin)
-       let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
-       
-       let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
-       
-        let directionRequest = MKDirections.Request()
-        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
-        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
-        directionRequest.transportType = .automobile
-        
-        let directions = MKDirections(request: directionRequest)
-        directions.calculate{(response, error) in
-            guard let directionResonse = response else{
-            if let error = error{
-                print("we have an error in getting directions==\(error.localizedDescription)")
-            }
-            return
-            }
-            let route = directionResonse.routes[0]
-            self.mapView.addOverlay(route.polyline, level: .aboveRoads)
-            let rect = route.polyline.boundingMapRect
-            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
-        }*/
- 
         self.mapView.delegate = self
     }
     
@@ -86,15 +56,6 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func render(_ location: CLLocation){
         let sourceLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
                                                 longitude: location.coordinate.longitude)
-//        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-//        let region = MKCoordinateRegion(center: coordinate,span: span)
-        
-//        self.mapView.setRegion(region,
-//                               animated: true)
-//        let pin = MKPointAnnotation()
-//        pin.coordinate = coordinate
-//        self.mapView.addAnnotation(pin)
-//         let sourceLocation = CLLocationCoordinate2D(latitude: 39.173209, longitude: -94.593933)
          let destinationLocation = CLLocationCoordinate2D(latitude: 37.3360, longitude: -121.9988)
          let sourcePin = customPin(pinTitle: "Me", pinSubTitle: "", location: sourceLocation)
          let destPin = customPin(pinTitle: "Kaiser Permanente", pinSubTitle: "", location: destinationLocation)
