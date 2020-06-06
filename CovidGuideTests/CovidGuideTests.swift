c9c775ba87ca0e50ca72e892c669c0056f0d3adf
srinjoy dutta <srinjoy.dutta@icloud.com>
2020-06-05 22:42:29 -0700

    Initial code

---
+++ CovidGuideTests/CovidGuideTests.swift
@@ -0,0 +1,34 @@
+//
+//  CovidGuideTests.swift
+//  CovidGuideTests
+//
+//  Created by Srinjoy Dutta on 6/5/20.
+//  Copyright © 2020 Srinjoy Dutta. All rights reserved.
+//
+
+import XCTest
+@testable import CovidGuide
+
+class CovidGuideTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
